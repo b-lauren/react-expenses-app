6be608bd38d4d47bb3d45a386eeffe7ec495f5de
Rebecca Ross <b.laurenross@gmail.com>
2021-04-26 23:39:39 +0100

    created income and transaction list components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { Income } from "./components/Income";
+import { TransactionList } from "./components/TransactionList";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Balance />
+        <Income />
+        <TransactionList />
+      </div>
     </div>
   );
 }
