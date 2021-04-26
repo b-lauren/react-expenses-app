4eba27dfb41bea6f97c0c109f0fdb000d19d7c54
Rebecca Ross <b.laurenross@gmail.com>
2021-04-27 00:21:18 +0100

    added useState hook

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { Income } from "./components/Income";
 import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Balance />
         <Income />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
